efd6bebc3083e671aea76b5ceae5d2f8c82a6570
sungchulyun <proattacker@naver.com>
2025-04-16 21:51:32 +0900

    init

---
+++ src/main/java/com/myfirstbank/finance_kids/MyfirstbankApplication.java
@@ -0,0 +1,13 @@
+package com.myfirstbank.finance_kids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstbankApplication.class, args);
+	}
+
+}
